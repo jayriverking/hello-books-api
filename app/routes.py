208bf1c5ce0323481c5933ef2a14b9aa1e824581
Jay King <jayriverking@gmail.com>
2023-04-25 08:30:32 -0400

    read all books chapter done

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,8 @@ from flask import Blueprint, jsonify, request, make_response
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
 @books_bp.route("", methods=["POST"])
-def handle_books():
+def create_books():
+
     request_body = request.get_json()
     new_book = Book(
         title=request_body["title"],
@@ -14,6 +15,19 @@ def handle_books():
     db.session.add(new_book)
     db.session.commit()
     return make_response(f"Book {new_book.title} successfully created", 201)
+
+@books_bp.route("", methods=["GET"])
+def get_all_books():
+    books = Book.query.all()
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
+    return jsonify(books_response)
+
     
 
 
@@ -30,7 +44,7 @@ def handle_books():
 # ]
 
 
-# @books_bp.route("/<book_id>")
+# @books_bp.route("", methods=["GET"])
 # def get_book(book_id):
 #     try:
 #         book_id = int(book_id)
@@ -43,4 +57,4 @@ def handle_books():
 #                 }
 #     except ValueError:
 #         return {"message": f"Book {book_id} is invalid"}, 400
-#     return {"message": f"Book {book_id} does not exist"}, 404
+    return {"message": f"Book {book_id} does not exist"}, 404
