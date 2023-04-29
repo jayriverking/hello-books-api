5113b396507dd01ec1b6f453eb792b5a44b60e61
Jay King <jayriverking@gmail.com>
2023-04-29 13:08:54 -0400

    add test all books with data, test one empty book

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -9,6 +9,21 @@ def test_get_all_books_with_no_records(client):
     assert response.status_code == 200
     assert response_body == []
 
+def test_get_all_books(client, two_saved_books):
+    response = client.get("/books")
+    response_body = response.get_json()
+    assert response.status_code == 200
+    assert response_body == [{
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }, 
+    {
+        "id": 2,
+        "title": "Mountain Book",
+        "description": "i luv 2 climb rocks"
+    }]
+
 def test_get_one_book(client, two_saved_books):
     # Act
     response = client.get("/books/1")
@@ -20,6 +35,11 @@ def test_get_one_book(client, two_saved_books):
         "title": "Ocean Book",
         "description": "watr 4evr"
     }
+
+def test_get_one_empty_book(client):
+    response = client.get("/books/1")
+    response_body = response.get_json()
+    assert response.status_code == 404
 
 def test_create_one_book(client):
     response = client.post("/books", json={
